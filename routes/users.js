a79084d3ce50efa28cc845fa7af2455c684316f8
mbcodes <mbcodes@protonmail.com>
2021-05-30 23:00:32 -0700

    routes updated

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router(); //brings in Router Object
 const authController = require('../controllers/auth');
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
+const expensesController = require('../controllers/expenses');
+const expenses = require('../controllers/expenses');
 
 //Login Page
 router.get('/login', ensureGuest, authController.getLogin);
@@ -10,12 +12,18 @@ router.get('/login', ensureGuest, authController.getLogin);
 router.get('/register', ensureGuest, authController.getRegister);
 
 // login handle; implements strategy
-router.post('/login', ensureGuest, authController.postLogin);
+router.post('/login', authController.postLogin);
 
 // Register handle
 router.post('/register', authController.postRegister);
 
 // logout handle
 router.get('/logout', authController.logout);
+
+// get expenses
+router.get('/:id', ensureAuth, expensesController.expensesGet);
+
+// expense(s) handle
+router.post('/expense', ensureAuth, expensesController.expensesPost);
 
 module.exports = router;
